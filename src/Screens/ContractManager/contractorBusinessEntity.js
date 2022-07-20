3bc29f4e0f0f5bd9f6071339dc5324226d725625
SivaSuriya0105 <sivasuriya0105@gmail.com>
2022-07-20 18:31:53 +0530

    pull commmit

--- src/Screens/ContractManager/contractorBusinessEntity.js
+++ src/Screens/ContractManager/contractorBusinessEntity.js
@@ -1,11 +1,45 @@
 import React, { useState } from 'react';
-import { InputGroup, RadioGroup, Radio } from '@blueprintjs/core';
+import { InputGroup, RadioGroup, Radio, Checkbox } from '@blueprintjs/core';
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import style from './index.module.scss';
 
 const ContractorBusinessEntity = ({getViewPage4, getCurrentPage, selectContractInfo}) => {
     const [sameAsContractor, setSameAsContractor] = useState(false);
+    const [contractorNPIN, setContractorNPIN] = useState({
+        notApplicable: true,
+        npin: "",
+        missing: true
+    });
+    const [contractorEntityTaxId, setContractorEntityTaxId] = useState({
+        taxId: "",
+        missing: true
+    });
+    const [businessEntity, setBusinessEntity] = useState({
+        name: ''
+    });
+    const [businessEntityUser, setBusinessEntityUser] = useState({
+        name: {
+            firstName: "",
+            lastName: "",
+            suffix: ""
+          },
+          email: {
+            officialEmail: ""
+          },
+          contactNumber: {
+            number: 0,
+            missing: true
+          }
+    });
+    const roles = [];
+    const [mailingAddress, setMailingAddress] = useState({
+        addressLine: "",
+        city: "",
+        state: "",
+        zipcode: ""
+    });
+    const [appRoleRequired, setAppRoleRequired] = useState(true);
 
     return (
         <div className={style.cloneBlockStyle}>
@@ -24,31 +58,29 @@ const ContractorBusinessEntity = ({getViewPage4, getCurrentPage, selectContractI
                 )}
                 <div className={`${style.extentionGrid} ${style.marginTop20}`}>
                     <div className={style.extentionLableStyle}>Business Entity Name*</div>
-                    <InputGroup className={style.fullWidth} value="Text" />
+                    <InputGroup className={style.fullWidth} 
+                    value={businessEntity?.name}
+                    onChange={(e) => setBusinessEntity({...businessEntity, name: e.target.value})} />
                 </div>
                 <div className={`${style.extentionGrid} ${style.marginTop20}`}>
                     <div className={style.extentionLableStyle}>Contractor NPIN*</div>
                     <div className={style.twoCol}>
-                        <InputGroup className={style.fullWidth} value={sameAsContractor ? "Value" : "Alphanumeric"} />
-                        <RadioGroup
-                            inline={true}
-                            className={`${style.marginTop} ${style.leftAlign}`}
-                        >
-                            <Radio label="Missing" value="Missing" />
-                            <Radio label="NA" value="NA" />
-                        </RadioGroup>
+                        <InputGroup className={style.fullWidth} value={contractorNPIN?.npin} 
+                         onChange={(e) => setContractorNPIN({...contractorNPIN, npin: e.target.value})}  />
+                        <div className={`${style.displayInRow} ${style.marginTop10}`}>
+                            <Checkbox label="Missing" checked={contractorNPIN?.missing} value="Missing" onChange={(e) => setContractorNPIN({...contractorNPIN, missing: e.target.checked})} />
+                            <Checkbox label="NA" checked={contractorNPIN?.notApplicable} value="NA" className={style.marginLeft20}
+                             onChange={(e) => setContractorNPIN({...contractorNPIN, notApplicable: e.target.checked})}  />
+                        </div>
                     </div>
                 </div>
                 <div className={`${style.extentionGrid} ${style.marginTop20}`}>
                     <div className={style.extentionLableStyle}>Contractor Entity Tax ID*</div>
                     <div className={style.twoCol}>
-                        <InputGroup className={style.fullWidth} value={sameAsContractor ? "Value" : "Alphanumeric"} />
-                        <RadioGroup
-                            inline={true}
-                            className={`${style.marginTop} ${style.leftAlign}`}
-                        >
-                            <Radio label="Missing" value="Missing" />
-                        </RadioGroup>
+                        <InputGroup className={style.fullWidth} value={contractorEntityTaxId?.taxId} 
+                        onChange={(e) => setContractorEntityTaxId({...contractorEntityTaxId, taxId: e.target.value})} />
+                        <Checkbox label="Missing" checked={contractorNPIN?.missing} value="Missing" className={style.marginLeft20}
+                        onChange={(e) => setContractorEntityTaxId({...contractorEntityTaxId, missing: e.target.checked})}  />
                     </div>
                 </div>
                 <div className={`${style.extentionGrid} ${style.marginTop20}`}>
