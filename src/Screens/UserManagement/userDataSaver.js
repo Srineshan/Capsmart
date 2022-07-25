ef32f3476a7383a4aefd691d0b2c8af826a965fe
keerthana1998G <keerthanag055@gmail.com>
2022-07-25 10:48:17 +0530

    auth token changed

--- src/Screens/UserManagement/userDataSaver.js
+++ src/Screens/UserManagement/userDataSaver.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Auth} from './../../utils/auth'
 import axios from "axios";
 
-const accessToken = Auth();
+// const accessToken = Auth();
+const accessToken = 'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjYyNDI4NTJlOTMzN2NkNTUzN2I4ODcxNSIsInVzZXJOYW1lIjoiSG9zcGl0YWwgMSIsInN1YiI6Imhvc3BpdGFsMUB0aW1lc21hcnRhaS5jb20iLCJpYXQiOjE2NTg3MjYxNzQsImV4cCI6MTY1ODgxMjU3NH0.VhljZO2Ci9xGdbk39WckKOrmmKSvAOHoQ46vS5AOw2cHluFybtusFwQSQbkCcstZyiRdHsDQHRQh2X5cIKrkCg';
 export const TenantID = '6242845f95690b3822cb96a5';
 const baseUrl = 'http://ec2-54-210-154-191.compute-1.amazonaws.com'
 const headers = {
