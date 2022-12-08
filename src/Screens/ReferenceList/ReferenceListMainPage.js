0e351153fdb8b7e27b82d91dcee66711ae419536
dhanapriya <dhanapriya055@gmail.com>
2022-12-08 16:50:04 +0530

    Reference list header component change by Leveltwo component used

--- src/Screens/ReferenceList/ReferenceListMainPage.js
+++ src/Screens/ReferenceList/ReferenceListMainPage.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { useLocation } from "react-router-dom";
-import ReferenceListMaster from "../../Components/ReferenceListMaster";
+import Navbar from "../../Components/Navbar";
+import SideBar from "../../Components/Sidebar";
 import industriesWithEntityTypes from "./industriesWithEntityTypes";
 import DepartmentsByEntityTypes from "./departmentsByEntityTypes";
 import SuffixByIndustries from "./suffixByIndustries";
@@ -11,12 +12,19 @@ import BoardCertification from "./boardCertification";
 import TerminationReasons from "./terminationReasons";
 import ProofOfDocumentationByEntity from "./proofOfDocumentationByEntity";
 import HolidayListByIndustries from "./holidayListByIndustries";
+import style from './index.module.scss';
+import LevelTwoHeader from "../../Components/LevelTwoHeader";
 
 const ReferenceListMainPage = () => {
     let location = useLocation();
 
     const [headerName, setHeaderName] = useState([]);
+    const [showAddEntityDialog, setShowAddEntityDialog] = useState(false);
+    const [isEdit, setIsEdit] = useState(false);
 
+    const getAddEntityDialog = (value) => {
+        setShowAddEntityDialog(value);
+    }
     const headerArray = [
         {
             name: "INDUSTRIES WITH ENTITY TYPES",
@@ -95,7 +103,41 @@ const ReferenceListMainPage = () => {
         getArrayHandler();
     }, [location.pathname]);
 
-    return <ReferenceListMaster headerName={headerName} />;
+    return (<Fragment>
+        <Navbar />
+        <div className={style.margin20}>
+            <div className={style.bigCardGrid}>
+                <SideBar />
+                <div>
+                    {
+                        headerName?.map((data) => (
+                            <>
+                                <LevelTwoHeader heading={data.name} updatedTime={data.status} getAddEntityDialog={getAddEntityDialog} isEdit={isEdit} setIsEdit={setIsEdit} needHeader={true} />
+                            </>
+                        ))
+                    }
+
+                    <div className={style.marginTop35}>
+                        <div className={style.centreCardStyle}>
+                            <div className={style.margin20}>
+                                {headerName.map((data) => {
+                                    return (
+                                        <data.tableComponent showAddEntityDialog={showAddEntityDialog} getAddEntityDialog={getAddEntityDialog} isEdit={isEdit} setIsEdit={setIsEdit} />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.spaceBetween}>
+                <p className={style.poweredBy}>Powered by - TimeSmart.AI LLP</p>
+                <p className={style.poweredBy}>© TimeSmart.AI</p>
+            </div>
+        </div>
+
+    </Fragment >
+    );
 };
 
 export default ReferenceListMainPage;
