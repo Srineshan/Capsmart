570f7049ac8ef74dc5b5ba16679909cceb524def
SivaSuriya0105 <sivasuriya0105@gmail.com>
2023-01-05 14:42:45 +0530

    feedback page added

--- src/Components/ConvertToPdf/index.js
+++ src/Components/ConvertToPdf/index.js
@@ -3,7 +3,8 @@ import domToPdf from "dom-to-pdf";
 const toPDF = (selector) => {
   const element = document.querySelector(selector);
   const options = {
-    filename: "test.pdf"
+    filename: "test.pdf",
+    proxyUrl: 'rest.timesmart.io'
   };
   domToPdf(element, options, function () {
     console.log("done");
