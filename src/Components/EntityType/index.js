35d30de8b238be3a396f00993a90a328825dd7ca
keerthana1998G <keerthanag055@gmail.com>
2023-02-15 10:32:21 +0530

    industry id changed

--- src/Components/EntityType/index.js
+++ src/Components/EntityType/index.js
@@ -1,31 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import {GET} from './../../Screens/dataSaver';
+import React, { useState, useEffect } from 'react';
+import { GET } from './../../Screens/dataSaver';
 import SelectField from './../SelectField';
 
-const EntityTypeList = ({onChangeFunc, value, className}) => {
+const EntityTypeList = ({ onChangeFunc, value, className, industryId }) => {
   const [entityTypeList, setEntityTypeList] = useState([]);
-  const industryId = "62ec0da4b188025da38b9d5e";
+  const [industry, setIndustry] = useState(industryId);
+  // const industryId = "62ec0da4b188025da38b9d5e";
 
-  useEffect(()=>{
+  useEffect(() => {
     getEntityTypeList();
-  },[])
+    setIndustry(industryId);
+  }, [])
 
-  const getEntityTypeList  = async() => {
-   await GET(`entity-service/entityTypeMaster?industryId=${industryId}`)
-   .then(response=>{
-     setEntityTypeList(response?.data);
-   })
-   .catch(error=>{
-     console.log('error',error);
-   })
- }
+  useEffect(() => {
+    setIndustry(industryId);
+  }, [industryId])
 
- const onSelectHandle = (id) => {
-   onChangeFunc(id,entityTypeList?.filter(data=>data?.id === id)?.map(data=>data?.type)[0]);
- }
+  const getEntityTypeList = async () => {
+    await GET(`entity-service/entityTypeMaster?industryId=${industry}`)
+      .then(response => {
+        setEntityTypeList(response?.data);
+      })
+      .catch(error => {
+        console.log('error', error);
+      })
+  }
 
-  return(
-    <SelectField className={className} value={value} selectLabel="Select Type" valueList={entityTypeList?.map(data=>data?.id)} dataList={entityTypeList} displayList={entityTypeList?.map(data=>data?.type)} onChangeFunc={onSelectHandle}/>
+  const onSelectHandle = (id) => {
+    onChangeFunc(id, entityTypeList?.filter(data => data?.id === id)?.map(data => data?.type)[0]);
+  }
+
+  return (
+    <SelectField className={className} value={value} selectLabel="Select Type" valueList={entityTypeList?.map(data => data?.id)} dataList={entityTypeList} displayList={entityTypeList?.map(data => data?.type)} onChangeFunc={onSelectHandle} />
   )
 }
 
