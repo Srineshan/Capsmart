e4d3590e5323cf2cf8703c9c5fdb1bf30907a044
keerthana1998G <keerthanag055@gmail.com>
2023-05-02 15:07:53 +0530

    backend url changed

--- src/Screens/SuperAdminDashboard/entitySystemAdmin.js
+++ src/Screens/SuperAdminDashboard/entitySystemAdmin.js
@@ -62,7 +62,7 @@ const EntitySystemAdmin = ({ getActiveStep }) => {
   }
 
   const getUserData = async () => {
-    // await axios(`https://${window.location.hostname}/user-management-service/user?partnerId=${partnerId}&userType=PARTNER_USER`, {
+    // await axios(`https://rest.mytimesmart.com/user-management-service/user?partnerId=${partnerId}&userType=PARTNER_USER`, {
     //   method: 'GET',
     //   headers: {
     //     'Content-Type': 'application/json',
