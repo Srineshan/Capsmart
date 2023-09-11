d2c84e19d18384bac52b2ed3c9b55a2b03448919
SivaSuriya0105 <sivasuriya0105@gmail.com>
2023-09-11 10:57:31 +0530

    recent code

--- src/Screens/Reports/reportTypeOverview.js
+++ src/Screens/Reports/reportTypeOverview.js
@@ -106,60 +106,49 @@ const ReportTypeOverview = () => {
     }, [addOnServicesValues]);
 
     useEffect(() => {
-        if (reportType === 'upcomingContractRenewals') {
-            getContractRenewalReport();
-        }
-        if (reportType === 'oneTimeContract') {
-            getOneTimeContract();
-        }
-        if (reportType === 'nonCompliant') {
-            getNonCompliantContractReportTile();
-        }
-        if (reportType === 'activitiesOrServices') {
-            getAcvityAndServices();
-        }
-        if (reportType === 'addOnActivities') {
-            getAddOnServices();
-        }
-        if (reportType === 'timesheetProcessingSummary') {
-            getTimesheetProcessingSummary('withoutParameter');
-        }
-        if (reportType === 'listingOfTimesheetsNotPaid') {
-            getListingOfTimesheetNotPaid('withoutParameter');
-        }
-        if (reportType === 'submittedTimesheetsPaymentStatus') {
-            getSubmittedTimesheetsPaymentStatus('withoutParameter');
-        }
-        if (reportType === 'paymentsProcessingSummary') {
-            getPayments();
-        }
-        if (reportType === 'compensationCostAnalysis') {
-            getCompensationCostAnalysis();
-        }
+        // if (reportType === 'upcomingContractRenewals') {
+        //     getContractRenewalReport();
+        // }
+        // if (reportType === 'oneTimeContract') {
+        //     getOneTimeContract();
+        // }
+        // if (reportType === 'nonCompliant') {
+        //     getNonCompliantContractReportTile();
+        // }
+        // if (reportType === 'activitiesOrServices') {
+        //     getAcvityAndServices();
+        // }
+        // if (reportType === 'addOnActivities') {
+        //     getAddOnServices();
+        // }
+        // if (reportType === 'timesheetProcessingSummary') {
+        //     getTimesheetProcessingSummary('withoutParameter');
+        // }
+        // if (reportType === 'listingOfTimesheetsNotPaid') {
+        //     getListingOfTimesheetNotPaid('withoutParameter');
+        // }
+        // if (reportType === 'submittedTimesheetsPaymentStatus') {
+        //     getSubmittedTimesheetsPaymentStatus('withoutParameter');
+        // }
+        // if (reportType === 'paymentsProcessingSummary') {
+        //     getPayments();
+        // }
+        // if (reportType === 'compensationCostAnalysis') {
+        //     getCompensationCostAnalysis();
+        // }
         getUsersData();
+        console.log('initial one');
     }, [])
 
     useEffect(() => {
-        // // setLoading(true);
-        // setTimeout(function () {
         getUpdatedValuesWithParams();
-        // }, 1000);
-    }, [dataToUseInReport, selectedPodTypeFromTile])
-    // }, [selectedPodTypeFromTile])
-
-    // useEffect(() => {
-    //     setTimeout(function () {
-    //         getIsRefresh(true);
-    //     }, 3000);
-    // }, [dataToUseInReport])
-
+    }, [selectedPodTypeFromTile, dataToUseInReport?.from, dataToUseInReport?.to, dataToUseInReport?.selectedContracts, dataToUseInReport?.selectedContractedServiceProvider, dataToUseInReport?.selectedSites, dataToUseInReport?.selectedDepartments])
 
     useEffect(() => {
         setApexStackedBarChartDisplay(<ApexStackedBarChart stackedSeries={stackedSeries} stackedCategories={stackedCategories} />);
     }, [stackedCategories, stackedSeries])
 
     const getUpdatedValuesWithParams = () => {
-        // setTimeout(function () {
         if (reportType === 'upcomingContractRenewals') {
             getContractRenewalReportWithParameters();
         }
@@ -194,9 +183,6 @@ const ReportTypeOverview = () => {
         if (reportType === 'submittedTimesheetsPaymentStatus') {
             getSubmittedTimesheetsPaymentStatus('withParameter');
         }
-        // setTimeout(function () {
-        //     getIsRefresh(true)
-        // }, 2000);
     }
 
     const getIsRefresh = (value) => {
