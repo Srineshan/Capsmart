08180917ad7a161a348962040384f6849b1d86b9
keerthana1998G <keerthanag055@gmail.com>
2023-11-07 08:14:25 +0530

    url change

--- src/Screens/SuperAdminDashboard/getSSOId.js
+++ src/Screens/SuperAdminDashboard/getSSOId.js
@@ -25,7 +25,7 @@ const GetSSOId = () => {
     }, [tenantId])
 
     const getEntityId = async () => {
-        await axios(`http://ec2-35-161-12-67.us-west-2.compute.amazonaws.com:8010/entity-service/entityID`, {
+        await axios(`http://ec2-34-230-167-131.compute-1.amazonaws.com:8010/entity-service/entityID`, {
             method: 'GET',
         }).then(response => {
             setTenantId(response?.data?.id);
