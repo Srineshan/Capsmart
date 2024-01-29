18103614ad0bd52fefc9cdc85798824f311c77ab
SivaSuriya0105 <sivasuriya0105@gmail.com>
2024-01-29 11:17:44 +0530

    compensation tracker

--- src/Screens/ContractManager/trackYourContracts.js
+++ src/Screens/ContractManager/trackYourContracts.js
@@ -56,41 +56,10 @@ const TrackYourContracts = () => {
 
     console.log(trackType)
 
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-            field: 'firstName',
-            headerName: 'First name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'lastName',
-            headerName: 'Last name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'age',
-            headerName: 'Age',
-            type: 'number',
-            width: 110,
-            editable: true,
-        },
-        {
-            field: 'fullName',
-            headerName: 'Full name',
-            description: 'This column has a value getter and is not sortable.',
-            sortable: false,
-            width: 160,
-            valueGetter: (params) =>
-                `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-    ];
 
     const getColumns = () => {
         let tempCol = [
-            { field: 'service', headerName: '', width: 200 },
+            { field: 'service', headerName: '', width: 280 },
             { field: 'hourlyRate', headerName: 'Pro-Rata Hourly Rate', width: 140 },
             { field: 'cyExpectedHours', headerName: 'Expected (Hours)', width: 140 },
             { field: 'cyExpectedAmount', headerName: 'Expected (Amount)', width: 140 },
@@ -374,9 +343,13 @@ const TrackYourContracts = () => {
                                             experimentalFeatures={{ columnGrouping: true }}
                                             className={style.whiteBackground}
                                             columnGroupingModel={getColumnGroupingModel()}
-                                            slots={{
-                                                toolbar: customToolbar,
-                                            }}
+                                            // slots={{
+                                            //     toolbar: customToolbar,
+                                            // }}
+                                            showCellVerticalBorder={true}
+                                            showColumnVerticalBorder={true}
+                                            rowHeight={35}
+                                            columnHeaderHeight={35}
                                         />
                                     )}
                                 </div>
