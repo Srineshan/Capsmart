b9bf704e811abbe1e8acea14aa4d46ca92ec332a
keerthana1998G <keerthanag055@gmail.com>
2024-04-05 13:20:36 +0530

    Approved Status changed

--- src/Screens/ContractManager/timeSheetSubmissionTerms.js
+++ src/Screens/ContractManager/timeSheetSubmissionTerms.js
@@ -504,10 +504,10 @@ const TimeSheetSubmissionTerms = ({ getViewPage7, getCurrentPage, contractId, is
                 "id": getSelectedUserDetails(reviewer)?.name?.suffix?.id,
                 "suffix": getSelectedUserDetails(reviewer)?.name?.suffix?.suffix,
               },
-              "workFlowStatus": {
-                "status": "APPROVED"
-              }
             },
+            "workFlowStatus": {
+              "status": "APPROVED"
+            }
           }
         }
       }
