cdd0e3a5f774a316d01767bb808d18143929d1e0
SivaSuriya0105 <SivaSuriya0105>
2024-04-11 18:36:04 +0530

    existing contract changes

--- src/Screens/ContractManager/addContract.js
+++ src/Screens/ContractManager/addContract.js
@@ -41,7 +41,7 @@ const AddContract = ({
 
   useEffect(() => {
     getContractTypeList();
-    getActiveContracts();
+    getRenewalContracts();
   }, [])
 
   const getContractTypeList = async () => {
@@ -51,9 +51,12 @@ const AddContract = ({
     setContractTypeList(contractType);
   };
 
-  const getActiveContracts = async () => {
-    const { data: contracts } = await GET(`contract-managment-service/contracts?limit=200&tab=activecontracts`);
-    setActiveContractList(contracts?.contractList);
+  const getRenewalContracts = async () => {
+    const { data: contracts } = await GET(`contract-managment-service/contracts?limit=200&tab=upcomingrenewals`);
+    let temp = [...contracts?.contractList] || [];
+    const { data: expiredContracts } = await GET(`contract-managment-service/contracts?limit=200&tab=expired/terminated`);
+    expiredContracts?.contractList?.map(data => { temp.push(data) })
+    setActiveContractList(temp);
   };
 
   const handleExistingContract = (id) => {
