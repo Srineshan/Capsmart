c87c57a2f7a5d0b3bf42240c9470d8488b526085
DhanapriyaUtsaha <Dhanapriya055@gmail.com>
2024-08-01 22:00:00 +0530

    Active staff & General Configuration Changes Updated

---
+++ src/Screens/ActiveStaff/activeStaffTiles.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import TileApplication from '../../Components/TileApplication';
+import style from './index.module.scss';
+import { GET } from '../dataSaver';
+
+const ActiveStaffTiles = ({ getSelectedTab, selectedTab }) => {
+
+  return (
+    <div className={`${style.tabs}`}>
+      <TileApplication selectedTab={selectedTab} getSelectedTab={getSelectedTab} tileLabel="Permanent Staff" tileCount={1} currentTile="permanentStaff" />
+      <TileApplication selectedTab={selectedTab} getSelectedTab={getSelectedTab} tileLabel="Provisional Staff" tileCount={1} currentTile="provisionalStaff" />
+    </div>
+  )
+}
+
+export default ActiveStaffTiles;
---
+++ src/Screens/ActiveStaff/index.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useState } from 'react';
+import Navbar from '../../Components/Navbar';
+import ActiveStaffList from './activeStaffList';
+
+const ActiveStaff = () => {
+    const [selectedTab, setSelectedTab] = useState('permanentStaff');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getSelectedTab = (value) => {
+        setSelectedTab(value);
+    }
+
+    return (
+        <Fragment>
+            <Navbar />
+            <ActiveStaffList
+                isLoading={isLoading}
+                getSelectedTab={getSelectedTab}
+                selectedTab={selectedTab}
+            />
+        </Fragment>
+    )
+}
+
+export default ActiveStaff;
