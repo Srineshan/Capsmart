0ad65ae049f44826e06ecc55ce5bd1df31990a6a
keerthana1998G <keerthanag055@gmail.com>
2024-09-11 18:58:44 +0530

    applicant flow added

--- src/Screens/Applicant/index.js
+++ src/Screens/Applicant/index.js
@@ -2,28 +2,51 @@ import React, { useEffect, useState, useRef } from 'react';
 import { GET, PUT } from '../dataSaver';
 import { KeyboardReturnRounded } from '@material-ui/icons';
 import { useNavigate } from 'react-router-dom';
-
+import Cookie from 'universal-cookie';
+import style from './index.module.scss';
+import jwt from 'jwt-decode';
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Applicant = () => {
     const navigate = useNavigate()
+    let cookie = new Cookie();
+    let userDetails = cookie.get('user');
+    const user = jwt(userDetails);
+    const [userId, setUserId] = useState(user?.id);
 
     const [applicationForms, setApplicationForms] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         getApplications();
     }, [])
 
     const getApplications = async () => {
-        setLoading(true);
+        setIsLoading(true);
         const { data: applications } = await GET(
-            `application-management-service/application`
+            `application-management-service/application?applicantId=${userId}`
         );
-        setApplicationForms(applications)
-        navigate(`applicationForm/`)
-        setLoading(false);
+        if (applications) {
+            navigate(`applicationForm/${applications?.[0]?.id}`)
+            setIsLoading(false);
+        }
+        // setApplicationForms(applications)
+        setIsLoading(false);
     }
 
-    KeyboardReturnRounded
+    return (
+        <div>
+            {
+                isLoading && (
+                    <div className={`${style.verticalAlignCenter} ${style.justifyCenter}`}>
+                        <CircularProgress sx={{ color: "#7165E3" }} />
+                    </div>
+                )
+            }
+        </div>
+
+    )
+
+
 }
 
 export default Applicant
