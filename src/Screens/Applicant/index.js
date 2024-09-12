ff21b7238a02f606939321b7819f35e802382daf
keerthana1998G <keerthanag055@gmail.com>
2024-09-12 20:09:43 +0530

    curent user data updated

--- src/Screens/Applicant/index.js
+++ src/Screens/Applicant/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { GET, PUT } from '../dataSaver';
 import { KeyboardReturnRounded } from '@material-ui/icons';
 import { useNavigate } from 'react-router-dom';
-import { Auth, GetEntityDetails, GetRoles, baseUrl } from "./../../utils/auth";
+import { Auth, GetEntityDetails, GetRoles, baseUrl, currentUser } from "./../../utils/auth";
 import axios from "axios";
 import { CookiesProvider, Cookies } from 'react-cookie';
 import style from './index.module.scss';
@@ -11,10 +11,12 @@ import CircularProgress from "@mui/material/CircularProgress";
 const accessToken = Auth();
 
 const Applicant = () => {
+    const currentUserData = currentUser();
     const navigate = useNavigate();
     const cookies = new Cookies();
-    let userDetails = cookies.get('user');
-    const user = jwt(userDetails);
+    // let userDetails = cookies.get('user');
+    // const user = jwt(userDetails);
+    console.log('currentUserData', currentUserData);
     const [userId, setUserId] = useState(user?.id);
 
     const [applicationForms, setApplicationForms] = useState([]);
@@ -27,19 +29,23 @@ const Applicant = () => {
     const getApplications = async () => {
         setIsLoading(true);
 
-        await axios(`${baseUrl}/application-management-service/application?applicantId=${user?.id}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "X-Authorization": `Bearer ${accessToken}`,
-            },
-        }).then(response => {
-            navigate(`/applicationForm/${response?.[0]?.id}`)
-            setIsLoading(false);
-            cookies.remove('entityId', { path: '/' })
-            cookies.set('entityId', response?.[0]?.tenant?.id, { path: '/' });
+        const { data: application } = await GET(
+            `application-management-service/application?applicantId=${currentUserData?.id}`
+        );
 
-        }).catch(err => { console.log('error', err) })
+        // await axios(`${baseUrl}/application-management-service/application?applicantId=${user?.id}`, {
+        //     method: "GET",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "X-Authorization": `Bearer ${accessToken}`,
+        //     },
+        // }).then(response => {
+        navigate(`/applicationForm/${application?.[0]?.id}`)
+        setIsLoading(false);
+        cookies.remove('entityId', { path: '/' })
+        cookies.set('entityId', application?.[0]?.tenant?.id, { path: '/' });
+
+        // }).catch(err => { console.log('error', err) })
 
         // const { data: applications } = await GET(
         //     `application-management-service/application?applicantId=${userId}`
