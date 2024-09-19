38ac95d443889f44575e5798ebd466393447b77c
keerthana1998G <keerthanag055@gmail.com>
2024-09-19 14:47:27 +0530

    privileges screen changes

---
+++ src/Screens/ApplicationForm/Step8/SaveInProgressDialog/index.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { Dialog, Classes } from '@blueprintjs/core';
+import CrossPink from "../../../../images/crossPink.png";
+import CommonSelectField from '../../../../Components/CommonFields/CommonSelectField';
+import { GET } from '../../../dataSaver';
+import { Icon } from '@blueprintjs/core';
+
+import style from './index.module.scss'
+
+const SaveInProgressDialog = ({ getIsOpen }) => {
+    const [isContinue, setIsContinue] = useState(false);
+    const [staffPrivilege, setStaffPrivilege] = useState([]);
+    const [selectedPrivilege, setSelectedPrivilege] = useState('');
+    const [collapsibleIndexes, setCollapsibleIndexes] = useState([]);
+
+    console.log('staffPrivilege', staffPrivilege)
+
+    useEffect(() => {
+        getStaffPrivilege();
+    }, [])
+
+    const getStaffPrivilege = async () => {
+        const { data: privilege } = await GET(
+            `entity-service/staffPrivilege`
+        );
+        setStaffPrivilege(privilege);
+    }
+
+    const handleChange = (privilegeId) => {
+        setSelectedPrivilege(privilegeId);
+    }
+
+    const handleCollapse = (value, index) => {
+        console.log('value', value, index)
+
+        let temp = collapsibleIndexes;
+        if (value === 'open') {
+            console.log('inside if_')
+            temp.push(index);
+            setCollapsibleIndexes(temp);
+
+        } else {
+            setCollapsibleIndexes(temp?.filter(data => data !== index)?.map(data => data));
+
+        }
+    }
+
+
+    return (
+        <Dialog isOpen={getIsOpen} onClose={() => getIsOpen(false)} className={`${style.eSignDialog} ${style.eSignDialogBackground}`} canOutsideClickClose={false} canEscapeKeyClose={false}>
+            <div>
+                <div className={Classes.DIALOG_BODY}>
+                    <div className={style.spaceBetween}>
+                        <div className={style.heading}>Requesting Additional Privileges</div>
+                        <div className={style.displayInRow}>
+                            <img
+                                src={CrossPink}
+                                alt="cross"
+                                className={`${style.crossStyle} ${style.cursorPointer} ${style.marginLeft} `}
+                                onClick={() => { getIsOpen(false) }}
+                            />
+                        </div>
+                    </div>
+                    <div >
+                        <div className={style.cardTitle}>{'Indicate the Privileges you are seeking as a(n) {Associate} for the {department anesthesiology / speciality}'}</div>
+                    </div>
+                    <CommonSelectField
+                        // value={getValueByPath(basicForm, `${basicpath}.${baseKey}.${fieldKey}`) || null}
+                        onChange={(e) => handleChange(e.target.value)}
+                        className={style.fullWidth}
+                        valueList={staffPrivilege?.map(data => data?.id) || []}
+                        labelList={staffPrivilege?.map(data => data?.privilegeSetTitle) || []}
+                        disabledList={[].map(data => false)}
+                        label={'Privilege Category'}
+                    // required={false}
+                    // warning={warningFields?.map(data => data?.key)?.includes(`${basicpath}.${baseKey}.${fieldKey}`)}
+                    />
+
+                    {
+                        staffPrivilege?.filter(data => data?.id === selectedPrivilege)?.map((data, index) => data?.privilegeDetails?.corePrivilegeDetails?.corePrivilegesByCategories?.map(categories => (
+                            <div className={style.marginTop}>
+                                <div>{categories?.category === null ? '' : categories?.category}</div>
+                                <div> {!collapsibleIndexes?.includes(`core${index}`) ? <Icon icon="chevron-down" className={`${style.margin} ${style.cursor} ${style.border} ${style.marginRight}`} onClick={() => handleCollapse('open', `core${index}`)} /> : <Icon icon="chevron-up" className={`${style.margin} ${style.cursor} ${style.border} ${style.marginRight}`} onClick={() => handleCollapse('close', `core${index}`)} />}
+                                </div>{
+                                    categories?.privileges?.map(privileges => (
+                                        <div className={style.twoColGrid}>
+                                            <div className={style.itemLeft}>{privileges?.privilegeId || ''}</div>
+                                            <div className={style.itemLeft}>{privileges?.title || ''}</div>
+                                        </div>
+
+                                    ))
+                                }
+                            </div>
+                        )
+
+                        )
+
+                        )
+                    }
+
+                    <div className={`${style.justifyCenter} ${style.displayInRow} ${style.marginTop}`}>
+                        <div className={`${style.continue} ${style.marginLeft}`} onClick={() => { getIsOpen(false); }}>ADD</div>
+                    </div>
+                </div>
+
+            </div>
+        </Dialog >
+    )
+}
+
+export default SaveInProgressDialog;
--- src/Screens/ApplicationForm/Step8/index.js
+++ src/Screens/ApplicationForm/Step8/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ProgressCard from '../../../Components/ProgressCard';
+import SaveInProgressDialog from './SaveInProgressDialog';
 import ApplicationUserCard from '../../../Components/ApplicationUserCard';
 import ApplicationAssistanceCard from '../../../Components/ApplicationAssistanceCard';
 import CommonDivider from '../../../Components/CommonFields/CommonDivider';
@@ -7,13 +8,35 @@ import ApplicationFieldCard from '../../../Components/ApplicationFieldCard';
 import ApplicationReferenceDocuments from '../../../Components/ApplicationReferenceDocuments';
 import { GET } from '../../dataSaver';
 import { useNavigate } from 'react-router-dom';
+import { Icon } from '@blueprintjs/core';
+import { format } from 'date-fns';
 
+import CryptoJS from 'crypto-js';
 import style from './index.module.scss';
 import CommonSelectField from '../../../Components/CommonFields/CommonSelectField';
+import ESignature from '../../../Components/ESignature';
 
 const Step8 = ({ basicForm, setBasicForm }) => {
+    const [isSigned, setIsSigned] = useState(false);
     const [formSchema, setFormSchema] = useState();
+    const [staffPrivilege, setStaffPrivilege] = useState([]);
+    const [selectedPrivilege, setSelectedPrivilege] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
+    let name = 'keerthana';
+    const publicKey = "-----BEGIN PUBLIC KEY-----MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHA5SDu30/8uQAqqkQE0NuY4ePBptMGufG6AWnC/88YVLXi4thh7M8VU6kElVJkfXL5DwlfVnwPb08+PK1EcaOWWtp2gdQitkohjZLB9zVE+0OtRrzSc33wItf7Iwisi5dHPggHvfOp5fr+QYWFMa/kKYl3SgNo8fryeLbKKalmdAgMBAAE=-----END PUBLIC KEY-----";
+    const [dateTime, setDateTime] = useState(new Date().toISOString());
+    const [isChecked, setIsChecked] = useState(false);
+    const [collapsibleIndexes, setCollapsibleIndexes] = useState([]);
+    const [encryptedText, setEncryptedText] = useState(CryptoJS.AES.encrypt(name + dateTime, publicKey).toString());
+    const [currentDate, setCurrentDate] = useState(format(new Date(), 'dd-MM-yyyy'));
+
+    // const [collapse]
     const navigate = useNavigate()
+
+    useEffect(() => {
+        getStaffPrivilege();
+    }, [])
+
     useEffect(() => {
         if (basicForm && !formSchema) {
             getFormSchema()
@@ -27,6 +50,16 @@ const Step8 = ({ basicForm, setBasicForm }) => {
         setFormSchema(form?.schema)
     }
 
+    const getStaffPrivilege = async () => {
+        const { data: privilege } = await GET(
+            `entity-service/staffPrivilege`
+        );
+        setStaffPrivilege(privilege);
+
+    }
+
+    console.log('staffPrivilege', staffPrivilege)
+
     const handleContinue = () => {
         if (sessionStorage.getItem('fromSummary') === "true") {
             navigate(-1);
@@ -37,9 +70,31 @@ const Step8 = ({ basicForm, setBasicForm }) => {
         }
     }
 
-    const handleChange = () => {
-
+    const getIsOpen = (value) => {
+        setIsOpen(value);
     }
+
+    const handleChange = (privilegeId) => {
+        setSelectedPrivilege(privilegeId);
+    }
+
+    const handleCollapse = (value, index) => {
+        console.log('value', value, index)
+
+        let temp = collapsibleIndexes;
+        if (value === 'open') {
+            console.log('inside if_')
+            temp.push(index);
+            setCollapsibleIndexes(temp);
+
+        } else {
+            setCollapsibleIndexes(temp?.filter(data => data !== index)?.map(data => data));
+
+        }
+    }
+
+    console.log('collapsibleIndexes', collapsibleIndexes)
+
     return (
         <div>
             <div className={style.applicationScreenGrid}>
@@ -52,19 +107,19 @@ const Step8 = ({ basicForm, setBasicForm }) => {
                         <div className={style.padding}>
                             <div className={style.cardTitle}>{'Indicate the Privileges you are seeking as a(n) {Associate} for the {department anesthesiology / speciality}'}</div>
                         </div>
-                        {/* <CommonSelectField
+                        <CommonSelectField
                             // value={getValueByPath(basicForm, `${basicpath}.${baseKey}.${fieldKey}`) || null}
-                            onChange={(e) => handleChange(fieldKey, e.target.value, baseKey)}
+                            onChange={(e) => handleChange(e.target.value)}
                             className={style.fullWidth}
                             // firstOptionLabel={fieldData.label}
                             // firstOptionValue={fieldData.label}
-                            valueList={[]}
-                            labelList={[]}
+                            valueList={staffPrivilege?.map(data => data?.id)}
+                            labelList={staffPrivilege?.map(data => data?.privilegeSetTitle)}
                             disabledList={[].map(data => false)}
                             label={'Privilege Category'}
                             required={false}
-                            warning={warningFields?.map(data => data?.key)?.includes(`${basicpath}.${baseKey}.${fieldKey}`)}
-                        /> */}
+                        // warning={warningFields?.map(data => data?.key)?.includes(`${basicpath}.${baseKey}.${fieldKey}`)}
+                        />
                         {/* {formSchema !== undefined && 'privilegeCategories' in formSchema?.properties && (
                             <ApplicationFieldCard object={formSchema?.properties?.privilegeCategories} gridStyle={style.privilegeGrid} baseKey={'privilegeCategories'} basicForm={basicForm} setBasicForm={setBasicForm} addMoreType={true} />
                         )}
@@ -72,7 +127,125 @@ const Step8 = ({ basicForm, setBasicForm }) => {
                             <ApplicationFieldCard object={formSchema?.properties?.additionalInformationAndSupportingDocuments} gridStyle={style.privilegeGrid} baseKey={'additionalInformationAndSupportingDocuments'} basicForm={basicForm} setBasicForm={setBasicForm} />
                         )} */}
                     </div>
+                    <div className={`${style.applicationCardStyle} ${style.marginTop}`} >
+                        <div className={style.padding}>
+                            <div className={style.cardTitle}>{'CAMBRIDGE MEMORIAL HOSPITAL GENERAL SURGERY PRIVILEGES'}</div>
+
+                            {
+                                staffPrivilege?.filter(data => data?.id === selectedPrivilege)?.map((data, index) => data?.privilegeDetails?.corePrivilegeDetails?.corePrivilegesByCategories?.map(categories => (
+                                    <div>
+                                        <div>{categories?.category === null ? '' : categories?.category}</div>
+                                        <div> {!collapsibleIndexes?.includes(`core${index}`) ? <Icon icon="chevron-down" className={`${style.margin} ${style.cursor} ${style.border} ${style.marginRight}`} onClick={() => handleCollapse('open', `core${index}`)} /> : <Icon icon="chevron-up" className={`${style.margin} ${style.cursor} ${style.border} ${style.marginRight}`} onClick={() => handleCollapse('close', `core${index}`)} />}
+                                        </div>{
+                                            categories?.privileges?.map(privileges => (
+                                                <div className={style.twoColGrid}>
+                                                    <div className={style.itemLeft}>{privileges?.privilegeId || ''}</div>
+                                                    <div className={style.itemLeft}>{privileges?.title || ''}</div>
+                                                </div>
+
+                                            ))
+                                        }
+                                    </div>
+                                )
+
+                                )
+
+                                )
+                            }
+                            <div className={style.twoCol}>
+                                <div
+                                    onClick={() => setIsSigned(!isSigned)}
+                                    className={!isChecked ? style.disabled : ''}
+                                >
+                                    <ESignature
+                                        userName={isSigned ? name : ""}
+                                        encData={isSigned ? encryptedText : ''}
+                                        showData={true}
+                                        showDatais={true}
+                                    />
+                                </div>
+                                <div className={style.verticalAlignCenter}>
+                                    <div className={style.displayInRow}>
+                                        <div className={style.dateTitle}>Date: </div>
+                                        <div className={`${style.date} ${style.marginLeft}`}>{isSigned ? currentDate : ""}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        {/* {formSchema !== undefined && 'privilegeCategories' in formSchema?.properties && (
+                            <ApplicationFieldCard object={formSchema?.properties?.privilegeCategories} gridStyle={style.privilegeGrid} baseKey={'privilegeCategories'} basicForm={basicForm} setBasicForm={setBasicForm} addMoreType={true} />
+                        )}
+                        {formSchema !== undefined && 'additionalInformationAndSupportingDocuments' in formSchema?.properties && (
+                            <ApplicationFieldCard object={formSchema?.properties?.additionalInformationAndSupportingDocuments} gridStyle={style.privilegeGrid} baseKey={'additionalInformationAndSupportingDocuments'} basicForm={basicForm} setBasicForm={setBasicForm} />
+                        )} */}
+                    </div>
+
+                    <div className={`${style.applicationCardStyle} ${style.marginTop}`} >
+                        <div className={style.padding}>
+                            <div className={style.cardDescription}>{'The following privileges are restricted and require evidence of qualification and competence. Continued competence would be evaluated as that being acceptable to the Medical Consultant of the Program. Please signify your intention regarding each privilege by marking and X opposite and sign below.'}</div>
+
+                            {
+                                staffPrivilege?.filter(data => data?.id === selectedPrivilege)?.map(data => data?.privilegeDetails?.restrictedPrivilegeDetails?.restrictedPrivilegesByCategory?.map(categories => (
+                                    <div>
+                                        <div>{categories?.category === null ? '' : categories?.category}</div>
+                                        <div> <Icon icon="chevron-down" className={`${style.margin} ${style.cursor} ${style.border} ${style.marginRight}`} />
+                                        </div>{
+                                            categories?.privileges?.map(privileges => (
+                                                <div className={style.twoColGrid}><div className={style.itemLeft}>{privileges?.privilegeId || ''}</div><div className={style.itemLeft}>{privileges?.title || ''}</div></div>
+
+                                            ))
+                                        }
+                                    </div>
+                                )
+
+                                )
+
+                                )
+                            }
+                            <div className={style.twoCol}>
+                                <div
+                                    onClick={isChecked ? () => { setIsSigned(!isSigned); setIsChecked(true) } : () => { }}
+                                    className={!isChecked ? style.disabled : ''}
+                                >
+                                    <ESignature
+                                        userName={isSigned ? name : ""}
+                                        encData={isSigned ? encryptedText : ''}
+                                        showData={true}
+                                        showDatais={true}
+                                    />
+                                </div>
+                                <div className={style.verticalAlignCenter}>
+                                    <div className={style.displayInRow}>
+                                        <div className={style.dateTitle}>Date: </div>
+                                        <div className={`${style.date} ${style.marginLeft}`}>{isSigned ? currentDate : ""}</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div> </div>
+
+                    <div className={`${style.marginTop} `}>
+                        <div className={`${style.alignCenter}`} onClick={() => setIsOpen(true)}>
+                            <div className={`${style.bigButtonStyle} `}>
+                                <div className={`${style.bigButtonTextStyle} ${style.alignCenter}`}>REQUEST ADDITIONAL PRIVILEGES</div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className={`${style.applicationCardStyle} ${style.marginTop}`} >
+                        <div className={style.padding}>
+                            <div className={style.cardDescription}>{'For specialties recognized by the Royal College of Physicians and Surgeons of Canada please attach a copy of a Royal College Certificate or a College Certificate of Registration permitting the practice of that sub-specialty.'}</div>
+                        </div>
+
+
+                    </div>
+
+
                 </div>
+                {isOpen && <SaveInProgressDialog getIsOpen={getIsOpen} staffPrivilege={staffPrivilege} />}
                 <div>
                     <ApplicationAssistanceCard user={'Neena Greenly'} designation={'{Designation}'} contactNumber={'{Contact Number}'} email={'{Email}'} />
                     <div className={`${style.saveInProgress} ${style.marginTop}`}>SAVE IN PROGRESS</div>
@@ -85,7 +258,7 @@ const Step8 = ({ basicForm, setBasicForm }) => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
