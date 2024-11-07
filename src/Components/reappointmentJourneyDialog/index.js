f4f2770c558af07d20442a2f06db3b30324c3f19
SivaSuriya0105 <SivaSuriya0105>
2024-11-07 15:03:03 +0530

    reappointment changes

--- src/Components/reappointmentJourneyDialog/index.js
+++ src/Components/reappointmentJourneyDialog/index.js
@@ -13,7 +13,7 @@ const ReappointmentJourneyDialog = ({ getIsOpen, title, basicForm, formIndex, im
     const [isContinue, setIsContinue] = useState(false);
     const { applicationId, section, step } = useParams();
     const [showSubmitDialog, setShowSubmitDialog] = useState(false);
-
+    const entityName = sessionStorage.getItem('title')
 
     const getIsShowSubmitDialog = (value) => {
         setShowSubmitDialog(value);
@@ -24,6 +24,7 @@ const ReappointmentJourneyDialog = ({ getIsOpen, title, basicForm, formIndex, im
         await POST(`application-management-service/application/${applicationId}/submit`)
             .then(response => {
                 console.log(response)
+                setShowSubmitDialog(true)
                 // SuccessToaster("Application Submitted Successfully");
             })
             .catch((error) => {
@@ -33,51 +34,50 @@ const ReappointmentJourneyDialog = ({ getIsOpen, title, basicForm, formIndex, im
     }
     return (
         <>
-        <Dialog isOpen={getIsOpen} onClose={() => getIsOpen(false)} className={`${style.eSignDialog} ${style.eSignDialogBackground}`} canOutsideClickClose={false} canEscapeKeyClose={false}>
-            <div>
-                <div className={Classes.DIALOG_BODY}>
-                    <div className={style.heading}>{title}</div>
-                    <div className={`${style.twoCol} ${style.marginTop}`}>
-                        <div className={style.verticalAlignCenter}>
-                            <img src={img} alt="step" className={style.journeyImg} />
-                        </div>
-                        <div className={style.verticalSpaceBetween}>
-                            <div>
-                                {basicForm?.forms?.map((data, index) => (formIndex >= index) && (
-                                    <div className={style.spaceBetween}>
-                                        <div className={style.completedItemsText}>{data?.title}</div>
-                                        <div><CheckCircleRoundedIcon style={{ fontSize: 20, color: `#25BF6A` }} /></div>
-                                    </div>
-                                ))}
+            <Dialog isOpen={getIsOpen} onClose={() => getIsOpen(false)} className={`${style.eSignDialog} ${style.eSignDialogBackground}`} canOutsideClickClose={false} canEscapeKeyClose={false}>
+                <div>
+                    <div className={Classes.DIALOG_BODY}>
+                        <div className={style.heading}>{title}</div>
+                        <div className={`${style.twoCol} ${style.marginTop}`}>
+                            <div className={style.verticalAlignCenter}>
+                                <img src={img} alt="step" className={style.journeyImg} />
                             </div>
-                            <div className={style.marginTop}>
-                                {(formIndex < basicForm?.forms?.length - 1) ? (
-                                    <>
-                                        <div className={style.completedItemsText}>NEXT</div>
-                                        <div className={style.nextItemText}>{basicForm?.forms[formIndex + 1]?.title}</div>
-                                        <div className={` ${style.displayInRow} ${style.marginTop}`}>
-                                            <div className={`${style.saveInProgress}`} onClick={() => { getIsOpen(false); }}>SAVE IN PROGRESS</div>
-                                            <div className={`${style.continue} ${style.marginLeft}`} onClick={() => { getIsOpen(false); continueClick() }}>CONTINUE</div>
+                            <div className={style.verticalSpaceBetween}>
+                                <div>
+                                    {basicForm?.forms?.map((data, index) => (formIndex >= index) && (
+                                        <div className={style.spaceBetween}>
+                                            <div className={style.completedItemsText}>{data?.title}</div>
+                                            <div><CheckCircleRoundedIcon style={{ fontSize: 20, color: `#25BF6A` }} /></div>
                                         </div>
-                                    </>
-                                ) : (
-                                    <div className={` ${style.displayInRow} ${style.marginTop}`}>
-                                        <div className={`${style.saveInProgress}`} onClick={() => { getIsOpen(false); logout() }}>LOGOUT</div>
-                                        {/* <div className={`${style.continue} ${style.marginLeft}`} onClick={() => { continueClick(); handleSubmitApplication() }}>SUBMIT</div> */}
-                                        <div className={`${style.continue} ${style.marginLeft}`} onClick={() => setShowSubmitDialog(true)}>SUBMIT</div>
-                                    </div>
-                                )}
+                                    ))}
+                                </div>
+                                <div className={style.marginTop}>
+                                    {(formIndex < basicForm?.forms?.length - 1) ? (
+                                        <>
+                                            <div className={style.completedItemsText}>NEXT</div>
+                                            <div className={style.nextItemText}>{basicForm?.forms[formIndex + 1]?.title}</div>
+                                            <div className={` ${style.displayInRow} ${style.marginTop}`}>
+                                                <div className={`${style.saveInProgress}`} onClick={() => { getIsOpen(false); }}>SAVE IN PROGRESS</div>
+                                                <div className={`${style.continue} ${style.marginLeft}`} onClick={() => { getIsOpen(false); continueClick() }}>CONTINUE</div>
+                                            </div>
+                                        </>
+                                    ) : (
+                                        <div className={` ${style.displayInRow} ${style.marginTop}`}>
+                                            <div className={`${style.saveInProgress}`} onClick={() => { getIsOpen(false); logout() }}>LOGOUT</div>
+                                            <div className={`${style.continue} ${style.marginLeft}`} onClick={() => { continueClick(); handleSubmitApplication() }}>SUBMIT</div>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
+            </Dialog >
 
-            </div>
-        </Dialog >
-
-        {showSubmitDialog && (
-                    <ApplicationSubmitDialog getIsOpen={getIsShowSubmitDialog} title={`Reappointment Application Submitted`} description={`Please note that the entire application process for full board approval may take up to 3 months to complete. The completed file will be forwarded to the credentials committee and medical advisory committee for review before being forwarded to the board of Cambridge Memorial Hospital for final consideration.`} />
-                )}
+            {showSubmitDialog && (
+                <ApplicationSubmitDialog getIsOpen={getIsShowSubmitDialog} title={`Reappointment Application Submitted`} description={`Please note that the entire application process for full board approval may take up to 3 months to complete. The completed file will be forwarded to the credentials committee and medical advisory committee for review before being forwarded to the board of ${entityName} for final consideration.`} />
+            )}
         </>
     )
 }
