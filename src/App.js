b63c4ea5456dc99dca119910d63e20b1fafe3d76
Siva Suriya G <sivasuriya0105@gmail.com>
2024-12-27 18:36:24 +0530

    recent code

--- src/App.js
+++ src/App.js
@@ -710,7 +710,7 @@ const App = ({ props }) => {
         method: "GET",
         headers: {
           "Authorization": `Bearer ${authorization}`,
-          "X-subdomain": 'acm-hospital',
+          "X-subdomain": 'master',
         },
       };
     console.log(requestHeader, 'requestHeader')
@@ -745,7 +745,7 @@ const App = ({ props }) => {
         "Content-Type": "application/json",
         "X-tenantID": id,
         "Authorization": `Bearer ${authorization}`,
-        "X-subdomain": 'acm-hospital',
+        "X-subdomain": 'master',
       },
     }
     fetch(`${baseUrl()}/user-management-service/auth/login`, requestOptions)
