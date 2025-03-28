b72af056ecb6cea532e3fde3728d61db6ccbcb25
aravinthanrs <aravinthan@paperz.co.in>
2025-03-28 19:29:07 +0530

    Dashboard Changes

--- src/App.js
+++ src/App.js
@@ -766,7 +766,7 @@ const App = ({ props }) => {
         method: "GET",
         headers: {
           "Authorization": `Bearer ${authorization}`,
-          "X-subdomain": 'master',
+          "X-subdomain": 'cmh-hospital',
         },
       };
     console.log(requestHeader, 'requestHeader')
@@ -806,7 +806,7 @@ const App = ({ props }) => {
         "Content-Type": "application/json",
         "X-tenantID": id,
         "Authorization": `Bearer ${authorization}`,
-        "X-subdomain": 'master',
+        "X-subdomain": 'cmh-hospital',
       },
     }
     fetch(`${baseUrl()}/user-management-service/auth/login`, requestOptions)
